ba668256f5b226acd2ed6ac523365a8aa5c9498b
Setu Gupta <setu@tamu.edu>
2025-04-09 17:38:23 -0500

    Added boilerplate code
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   inc/wait_free_bag.hpp
    	modified:   test/main.cpp

--- inc/wait_free_bag.hpp
+++ inc/wait_free_bag.hpp
@@ -1,6 +1,59 @@
+#include <functional>
 #include <iostream>
+#include <mutex>
+#include <optional>
+#include <syncstream>
+#include <thread>
 
-void foo()
+template<typename Func>
+concept invokable = requires(Func foo)
 {
-        std::cout << "Hello World\n";
-}
+        foo();
+};
+
+template<typename DataType, size_t ThreadCount>
+struct bag_data
+{
+                // TODO
+
+                // Ref: https://stackoverflow.com/questions/3069255/singleton-multi-threading
+                static bag_data& get_instance()
+                {
+                        static bag_data obj;
+                        return obj;
+                }
+};
+
+template<typename DataType, size_t ThreadCount>
+class bag
+{
+        private:
+                std::thread::id                 my_id;
+                bag_data<DataType, ThreadCount> bag_impl;
+
+        public:
+                bag(): my_id(std::hash<std::thread::id>()(std::this_thread::get_id()) % ThreadCount), bag_impl(bag_data<DataType, ThreadCount>::get_instance()) {}
+
+                void insert(DataType element)
+                {
+                        // TODO
+                }
+
+                std::optional<DataType> extract()
+                {
+                        // TODO
+                        return {};
+                }
+
+                std::size_t size()
+                {
+                        // TODO
+                        return 0;
+                }
+
+                void for_all(invokable auto f)
+                {
+                        f();
+                        // TODO
+                }
+};
--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,16 @@
+#include <iostream>
+#include <syncstream>
+#include <thread>
 #include <wait_free_bag.hpp>
+
+void foo()
+{
+        bag<int, 2> bg;
+        std::osyncstream(std::cout) << "Hi from " << std::this_thread::get_id() << '\n';
+}
 
 int main()
 {
-        foo();
+         std::jthread t1 {foo};
+         std::jthread t2 {foo};
 }
