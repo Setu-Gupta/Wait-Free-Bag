57141167a5faedd80b307bab79caf834c79e1f2b
Setu Gupta <setu.gupta.2020@gmail.com>
2025-04-13 22:12:36 -0500

    Added a test case
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   test/main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
+#include <omp.h>
 #include <syncstream>
-#include <thread>
+#include <vector>
 #include <wait_free_bag.hpp>
 
-void foo()
+void insert_test(wait_free_bag::WaitFreeBag<int, 1>& bag)
 {
-        bag<int, 2> bg;
-        std::osyncstream(std::cout) << "Hi from " << std::this_thread::get_id() << '\n';
+#pragma omp parallel for
+        for(int i = 0; i < 1; i++)
+        {
+                for(int j = 0; j < 10; j++) bag.insert(j);
+        }
+}
+
+void extract_test([[maybe_unused]] wait_free_bag::WaitFreeBag<int, 1>& bag)
+{
+        // TODO
+}
+
+void for_all_test(wait_free_bag::WaitFreeBag<int, 1>& bag)
+{
+        auto printer = [](int value)
+        {
+                std::osyncstream(std::cout) << "Value = " << value << '\n';
+        };
+        bag.for_all(printer);
 }
 
 int main()
 {
-         std::jthread t1 {foo};
-         std::jthread t2 {foo};
+        wait_free_bag::WaitFreeBag<int, 1> bag;
+
+        insert_test(bag);
+        extract_test(bag);
+        for_all_test(bag);
 }
