09223a410b3ca5d3d00d1a2b0ca2664c07ad9ff0
Setu Gupta <setu.gupta.2020@gmail.com>
2025-04-13 22:03:54 -0500

    Updated data types
    The code crashes
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   inc/wait_free_bag.hpp

--- inc/wait_free_bag.hpp
+++ inc/wait_free_bag.hpp
@@ -2,10 +2,9 @@
 
 #include <array>
 #include <atomic>
-#include <iostream>
 #include <omp.h>
 #include <optional>
-#include <syncstream>
+#include <stdexcept>
 #include <thread>
 
 namespace wait_free_bag
@@ -21,61 +20,65 @@ namespace wait_free_bag
                         struct node_t
                         {
                                 public:
-                                        DataType data;
-                                        node_t*  next;
+                                        DataType             data;
+                                        std::atomic<node_t*> next;
                         };
 
-                        node_t* head;
-                        node_t* tail;
+                        std::atomic<node_t*> head;
+                        std::atomic<node_t*> tail;
 
-                        const node_t* mask = 0x0000ffffffffffff;
+                        const size_t mask = 0x0000ffffffffffff;
 
                 public:
                         WaitFreeQueue()
                         {
                                 node_t* node = new node_t();
-                                node->next   = nullptr;
-                                head         = node;
-                                tail         = node;
+                                node->next.store(nullptr);
+                                head.store(node);
+                                tail.store(node);
                         }
 
                         bool enqueue(DataType data)
                         {
                                 // Set up the new node
                                 node_t* node = new node_t();
-                                if(!node || ((node & mask) != node)) return false;
+                                if(!node || ((reinterpret_cast<size_t>(node) & mask) != reinterpret_cast<size_t>(node))) return false;
                                 node->data = data;
-                                node->next = nullptr;
+                                node->next.store(nullptr);
 
                                 node_t* tail_copy = nullptr;
                                 while(true)
                                 {
-                                        tail_copy          = tail;
-                                        const node_t* next = tail->next;
-                                        if(tail == tail_copy)
+                                        tail_copy    = tail.load();
+                                        node_t* next = (tail.load()->next).load();
+                                        if(tail.load() == tail_copy)
                                         {
-                                                if((next & mask) == nullptr) // Add the new node
+                                                if((reinterpret_cast<size_t>(next) & mask) == 0x0) // Add the new node
                                                 {
-                                                        const node_t* node_ptr = node | 0x0001000000000000;
-                                                        node_t*       old_next = nullptr;
-                                                        if(std::atomic_compare_exchange_weak(&(tail->next), &next, node_ptr)) break;
+                                                        const node_t*              node_ptr = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(node) | 0x0001000000000000);
+                                                        const std::atomic<node_t*> atomic_node_ptr(const_cast<node_t*>(node_ptr));
+                                                        if(std::atomic_compare_exchange_weak(&(tail.load()->next), &next, atomic_node_ptr)) break;
                                                 }
                                                 else // Move the tail forward
                                                 {
-                                                        size_t count           = (tail_copy & mask) >> 48;
-                                                        count                  = (count + 1) & 0x000000000000ffff;
-                                                        count                  = (count << 48) & mask;
-                                                        const node_t* new_tail = next | count;
+                                                        size_t int_tail_copy = reinterpret_cast<size_t>(tail_copy);
+                                                        size_t count         = (int_tail_copy & mask) >> 48;
+                                                        count                = (count + 1) & 0x000000000000ffff;
+                                                        count                = (count << 48) & mask;
+                                                        node_t* new_tail     = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(next) | count);
                                                         std::atomic_compare_exchange_weak(&tail, &tail_copy, new_tail);
                                                 }
                                         }
                                 }
 
-                                size_t count           = (tail_copy & mask) >> 48;
-                                count                  = (count + 1) & 0x000000000000ffff;
-                                count                  = (count << 48) & mask;
-                                const node_t* new_tail = node | count;
+                                size_t int_tail_copy = reinterpret_cast<size_t>(tail_copy);
+                                size_t count         = (int_tail_copy & mask) >> 48;
+                                count                = (count + 1) & 0x000000000000ffff;
+                                count                = (count << 48) & mask;
+                                node_t* new_tail     = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(node) | count);
                                 std::atomic_compare_exchange_weak(&tail, &tail_copy, new_tail);
+
+                                return true;
                         }
 
                         std::optional<DataType> dequeue()
@@ -118,6 +121,18 @@ namespace wait_free_bag
                                 free(head_copy);
                                 return value;
                         }
+
+                        template<typename Func>
+                                requires invokable<Func, DataType>
+                        void for_all(Func f)
+                        {
+                                std::atomic<node_t*> iterator(head.load());
+                                while(iterator != tail)
+                                {
+                                        f(iterator.load()->next.load()->data);
+                                        iterator.store(iterator.load()->next);
+                                }
+                        }
         };
 
         template<typename DataType, size_t Spread>
@@ -130,17 +145,22 @@ namespace wait_free_bag
                 public:
                         void insert(DataType element)
                         {
-                                static thread_local int idx = omp_get_thread_num() % Spread;
-                                data[idx].enqueue(data);
-                                idx = (idx + 1) % Spread;
+                                static thread_local int idx     = omp_get_thread_num() % Spread;
+                                bool                    success = data[idx].enqueue(element);
+                                idx                             = (idx + 1) % Spread;
 
                                 std::atomic_fetch_add(&num_elements, 1);
+
+                                if(!success)
+                                {
+                                        throw std::logic_error("Could not insert object\n");
+                                }
                         }
 
                         std::optional<DataType> extract()
                         {
                                 static thread_local int idx     = omp_get_thread_num() % Spread;
-                                std::optional<DataType> element = data[idx].enqueue(data);
+                                std::optional<DataType> element = data[idx].dequeue();
                                 idx                             = (idx + 1) % Spread;
 
                                 if(element) std::atomic_fetch_sub(&num_elements, 1);
@@ -148,7 +168,7 @@ namespace wait_free_bag
                                 return element;
                         }
 
-                        size_t size()
+                        size_t size() const
                         {
                                 return static_cast<size_t>(num_elements);
                         }
@@ -158,10 +178,13 @@ namespace wait_free_bag
                         void for_all(Func f)
                         {
                                 #pragma omp barrier
-                                thread_local int idx = omp_get_thread_num() % Spread;
+                                thread_local int idx = omp_get_thread_num();
 
-                                // TODO
-
+                                while(static_cast<size_t>(idx) < Spread)
+                                {
+                                        data[idx].for_all(f);
+                                        idx = (idx + omp_get_num_threads());
+                                }
                                 #pragma omp barrier
                         }
         };
