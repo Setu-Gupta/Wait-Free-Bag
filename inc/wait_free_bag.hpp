de27c3b2a9ecf06dbfacb0d392c046f01433c580
Setu Gupta <setu.gupta.2020@gmail.com>
2025-04-15 14:41:49 -0500

    Changed size_t to std::size_t and improved invokable
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   inc/wait_free_bag.hpp

--- inc/wait_free_bag.hpp
+++ inc/wait_free_bag.hpp
@@ -8,7 +8,7 @@
 namespace wait_free_bag
 {
         template<typename Func, typename DataType>
-        concept invokable = requires(Func foo, DataType elem) { foo(elem); };
+        concept invokable = requires(Func foo, DataType elem) { foo(std::ref(elem)); };
 
         // Ref: https://www.cs.rochester.edu/~scott/papers/1996_PODC_queues.pdf
         template<typename DataType>
@@ -25,7 +25,7 @@ namespace wait_free_bag
                         std::atomic<node_t*> head;
                         std::atomic<node_t*> tail;
 
-                        const size_t mask = 0x0000ffffffffffff;
+                        const std::size_t mask = 0x0000ffffffffffff;
 
                 public:
                         WaitFreeQueue()
@@ -40,7 +40,7 @@ namespace wait_free_bag
                         {
                                 // Set up the new node
                                 node_t* const node = new node_t();
-                                if(!node || ((reinterpret_cast<size_t>(node) & mask) != reinterpret_cast<size_t>(node))) return false;
+                                if(!node || ((reinterpret_cast<std::size_t>(node) & mask) != reinterpret_cast<std::size_t>(node))) return false;
                                 node->data = std::move(data);
                                 node->next.store(nullptr);
 
@@ -48,32 +48,32 @@ namespace wait_free_bag
                                 while(true)
                                 {
                                         tail_copy                   = tail.load();
-                                        node_t* const tail_copy_ptr = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(tail_copy) & mask);
+                                        node_t* const tail_copy_ptr = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(tail_copy) & mask);
                                         node_t*       next          = (tail_copy_ptr->next).load();
                                         if(tail.load() == tail_copy)
                                         {
-                                                if((reinterpret_cast<size_t>(next) & mask) == 0x0) // Add the new node
+                                                if((reinterpret_cast<std::size_t>(next) & mask) == 0x0) // Add the new node
                                                 {
-                                                        node_t* const node_ptr = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(node) | 0x0001000000000000);
+                                                        node_t* const node_ptr = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(node) | 0x0001000000000000);
                                                         if(std::atomic_compare_exchange_weak(&(tail_copy_ptr->next), &next, node_ptr)) break;
                                                 }
                                                 else // Move the tail forward
                                                 {
-                                                        const size_t int_tail_copy = reinterpret_cast<size_t>(tail_copy);
-                                                        size_t       count         = (int_tail_copy & mask) >> 48;
+                                                        const std::size_t int_tail_copy = reinterpret_cast<std::size_t>(tail_copy);
+                                                        std::size_t       count         = (int_tail_copy & mask) >> 48;
                                                         count                      = (count + 1) & 0x000000000000ffff;
                                                         count                      = (count << 48) & mask;
-                                                        node_t* const new_tail     = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(next) | count);
+                                                        node_t* const new_tail     = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(next) | count);
                                                         std::atomic_compare_exchange_weak(&tail, &tail_copy, new_tail);
                                                 }
                                         }
                                 }
 
-                                const size_t int_tail_copy = reinterpret_cast<size_t>(tail_copy);
-                                size_t       count         = (int_tail_copy & mask) >> 48;
+                                const std::size_t int_tail_copy = reinterpret_cast<std::size_t>(tail_copy);
+                                std::size_t       count         = (int_tail_copy & mask) >> 48;
                                 count                      = (count + 1) & 0x000000000000ffff;
                                 count                      = (count << 48) & mask;
-                                node_t* const new_tail     = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(node) | count);
+                                node_t* const new_tail     = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(node) | count);
                                 std::atomic_compare_exchange_weak(&tail, &tail_copy, new_tail);
 
                                 return true;
@@ -87,39 +87,39 @@ namespace wait_free_bag
                                 while(true)
                                 {
                                         head_copy                         = head.load();
-                                        const node_t* const head_copy_ptr = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(head_copy) & mask);
+                                        const node_t* const head_copy_ptr = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(head_copy) & mask);
                                         node_t*             tail_copy     = tail.load();
-                                        const node_t* const tail_copy_ptr = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(tail_copy) & mask);
+                                        const node_t* const tail_copy_ptr = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(tail_copy) & mask);
                                         const node_t* const next          = head_copy_ptr->next;
-                                        const node_t* const next_ptr      = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(next) & mask);
+                                        const node_t* const next_ptr      = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(next) & mask);
                                         if(head_copy == head.load())
                                         {
                                                 // Check if the queue is empty or if the tail is lagging behind
                                                 if(head_copy_ptr == tail_copy_ptr)
                                                 {
-                                                        if((reinterpret_cast<size_t>(next) & mask) == 0x0) // Queue is empty
+                                                        if((reinterpret_cast<std::size_t>(next) & mask) == 0x0) // Queue is empty
                                                                 return {};
 
                                                         // Advance the tail
-                                                        size_t count           = (reinterpret_cast<size_t>(tail_copy_ptr) & mask) >> 48;
+                                                        std::size_t count           = (reinterpret_cast<std::size_t>(tail_copy_ptr) & mask) >> 48;
                                                         count                  = (count + 1) & 0x000000000000ffff;
                                                         count                  = (count << 48) & mask;
-                                                        node_t* const new_tail = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(next) | count);
+                                                        node_t* const new_tail = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(next) | count);
                                                         std::atomic_compare_exchange_weak(&tail, &tail_copy, new_tail);
                                                 }
 
                                                 value = std::move(next_ptr->data);
 
                                                 // Advance the head
-                                                size_t count           = (reinterpret_cast<size_t>(head_copy_ptr) & mask) >> 48;
+                                                std::size_t count           = (reinterpret_cast<std::size_t>(head_copy_ptr) & mask) >> 48;
                                                 count                  = (count + 1) & 0x000000000000ffff;
                                                 count                  = (count << 48) & mask;
-                                                node_t* const new_head = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(next) | count);
+                                                node_t* const new_head = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(next) | count);
                                                 if(std::atomic_compare_exchange_weak(&head, &head_copy, new_head)) break;
                                         }
                                 }
 
-                                const node_t* const head_copy_ptr = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(head_copy) & mask);
+                                const node_t* const head_copy_ptr = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(head_copy) & mask);
                                 delete head_copy_ptr;
                                 return value;
                         }
@@ -128,41 +128,41 @@ namespace wait_free_bag
                                 requires invokable<Func, DataType>
                         void for_all(Func f)
                         {
-                                const node_t* const  tail_ptr = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(tail.load()) & mask);
+                                const node_t* const  tail_ptr = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(tail.load()) & mask);
                                 std::atomic<node_t*> iterator(head.load());
-                                const node_t*        iterator_ptr = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(iterator.load()) & mask);
+                                const node_t*        iterator_ptr = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(iterator.load()) & mask);
                                 while(iterator_ptr != tail_ptr)
                                 {
                                         const node_t* const raw_next_ptr = (iterator_ptr->next).load();
-                                        node_t* const       next_ptr     = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(raw_next_ptr) & mask);
+                                        node_t* const       next_ptr     = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(raw_next_ptr) & mask);
 
                                         f(next_ptr->data);
                                         iterator.store(next_ptr);
-                                        iterator_ptr = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(iterator.load()) & mask);
+                                        iterator_ptr = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(iterator.load()) & mask);
                                 }
                         }
 
                         ~WaitFreeQueue()
                         {
-                                const node_t* const  tail_ptr = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(tail.load()) & mask);
+                                const node_t* const  tail_ptr = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(tail.load()) & mask);
                                 std::atomic<node_t*> iterator(head.load());
-                                const node_t*        iterator_ptr = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(iterator.load()) & mask);
+                                const node_t*        iterator_ptr = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(iterator.load()) & mask);
                                 while(iterator_ptr != tail_ptr)
                                 {
                                         const node_t* const raw_next_ptr = (iterator_ptr->next).load();
-                                        node_t* const       next_ptr     = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(raw_next_ptr) & mask);
+                                        node_t* const       next_ptr     = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(raw_next_ptr) & mask);
 
                                         delete iterator_ptr;
                                         iterator.store(next_ptr);
 
-                                        iterator_ptr = reinterpret_cast<node_t*>(reinterpret_cast<size_t>(iterator.load()) & mask);
+                                        iterator_ptr = reinterpret_cast<node_t*>(reinterpret_cast<std::size_t>(iterator.load()) & mask);
                                 }
 
                                 delete tail_ptr;
                         }
         };
 
-        template<typename DataType, size_t Spread>
+        template<typename DataType, std::size_t Spread>
         class WaitFreeBag
         {
                 private:
@@ -189,7 +189,7 @@ namespace wait_free_bag
                                 thread_local int idx = omp_get_thread_num() % Spread;
 
                                 std::optional<DataType> element;
-                                for(int i = 0; static_cast<size_t>(i) < Spread; i++)
+                                for(int i = 0; static_cast<std::size_t>(i) < Spread; i++)
                                 {
                                         element = data[idx].dequeue();
                                         idx     = (idx + 1) % Spread;
@@ -200,9 +200,9 @@ namespace wait_free_bag
                                 return element;
                         }
 
-                        size_t size() const
+                        std::size_t size() const
                         {
-                                return static_cast<size_t>(num_elements);
+                                return static_cast<std::size_t>(num_elements);
                         }
 
                         template<typename Func>
@@ -212,7 +212,7 @@ namespace wait_free_bag
                                 #pragma omp barrier
                                 int idx = omp_get_thread_num();
 
-                                while(static_cast<size_t>(idx) < Spread)
+                                while(static_cast<std::size_t>(idx) < Spread)
                                 {
                                         data[idx].for_all(f);
                                         idx = (idx + omp_get_num_threads());
