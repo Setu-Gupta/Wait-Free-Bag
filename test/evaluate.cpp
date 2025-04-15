e1bf50991e2f9354323ff5d00644b217ef73deed
Setu Gupta <setu.gupta.2020@gmail.com>
2025-04-15 15:32:13 -0500

    Added performance evaluation code
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   test/evaluate.cpp

--- test/evaluate.cpp
+++ test/evaluate.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <iostream>
 #include <omp.h>
+#include <print>
 #include <thread>
 #include <vector>
 #include <wait_free_bag.hpp>
@@ -52,8 +53,11 @@ void lock_based_extract(auto& vec)
         #pragma omp parallel for
         for(std::size_t i = 0; i < vec.size(); i++)
         {
-                #pragma omp critical
-                vec.pop_back();
+                while(vec.size() > 0)
+                {
+                        #pragma omp critical
+                        if(vec.size() > 0) vec.pop_back();
+                }
         }
 }
 
@@ -62,14 +66,44 @@ void wait_free_extract(auto& bag)
         #pragma omp parallel for
         for(int i = 0; i < 1024; i++)
         {
-                for(int j = 0; j < 2048; j++) bag.extract();
+                while(bag.size() > 0) bag.extract();
         }
 }
 
 int main()
 {
-        wait_free_bag::WaitFreeBag<int, 32> bag;
-        std::vector<int>                    vec;
+        wait_free_bag::WaitFreeBag<int, 1024> bag;
+        std::vector<int>                      vec;
 
-        // TODO
+        const auto tp0 = std::chrono::high_resolution_clock::now();
+        lock_based_insert(vec);
+        const auto tp1 = std::chrono::high_resolution_clock::now();
+        wait_free_insert(bag);
+        const auto tp2 = std::chrono::high_resolution_clock::now();
+        lock_based_for_all(vec);
+        const auto tp3 = std::chrono::high_resolution_clock::now();
+        wait_free_for_all(bag);
+        const auto tp4 = std::chrono::high_resolution_clock::now();
+        lock_based_extract(vec);
+        const auto tp5 = std::chrono::high_resolution_clock::now();
+        wait_free_extract(bag);
+        const auto tp6 = std::chrono::high_resolution_clock::now();
+
+        const std::chrono::duration<double> lock_based_insert_time  = tp1 - tp0;
+        const std::chrono::duration<double> wait_free_insert_time   = tp2 - tp1;
+        const std::chrono::duration<double> lock_based_for_all_time = tp3 - tp2;
+        const std::chrono::duration<double> wait_free_for_all_time  = tp4 - tp3;
+        const std::chrono::duration<double> lock_based_extract_time = tp5 - tp4;
+        const std::chrono::duration<double> wait_free_extract_time  = tp6 - tp5;
+
+        std::println("{},{},{},{},{},{},{},{},{}",
+                     lock_based_insert_time,
+                     wait_free_insert_time,
+                     lock_based_for_all_time,
+                     wait_free_for_all_time,
+                     lock_based_extract_time,
+                     wait_free_extract_time,
+                     lock_based_insert_time / wait_free_insert_time,
+                     lock_based_for_all_time / wait_free_for_all_time,
+                     lock_based_extract_time / wait_free_extract_time);
 }
