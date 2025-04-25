d9dd8bcc786d3f082b536513b6834d9a5cc63695
Setu Gupta <setu.gupta.2020@gmail.com>
2025-04-25 11:04:57 -0500

    Modified evaluation code for experimentation
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   test/evaluate.cpp

--- test/evaluate.cpp
+++ test/evaluate.cpp
@@ -1,48 +1,46 @@
 #include <chrono>
 #include <omp.h>
 #include <print>
+#include <sstream>
 #include <vector>
 #include <wait_free_bag.hpp>
 
-const int THREADS             = 16;
-const int ELEMENTS_PER_THREAD = 65536 * 32;
-
-void lock_based_insert(auto& vec)
+void lock_based_insert(auto& vec, const std::size_t num_threads, const std::size_t elements_per_thread)
 {
 	#pragma omp parallel for
-        for(int i = 0; i < THREADS; i++)
+        for(std::size_t i = 0; i < num_threads; i++)
         {
-                for(int j = 0; j < ELEMENTS_PER_THREAD; j++)
+                for(std::size_t j = 0; j < elements_per_thread; j++)
                 {
 	                #pragma omp critical
-                        vec.push_back((i * ELEMENTS_PER_THREAD) + j);
+                        vec.push_back((i * elements_per_thread) + j);
                 }
         }
 }
 
-void wait_free_insert(auto& bag)
+void wait_free_insert(auto& bag, const std::size_t num_threads, const std::size_t elements_per_thread)
 {
 	#pragma omp parallel for
-        for(int i = 0; i < THREADS; i++)
+        for(std::size_t i = 0; i < num_threads; i++)
         {
-                for(int j = 0; j < ELEMENTS_PER_THREAD; j++) bag.insert((i * ELEMENTS_PER_THREAD) + j);
+                for(std::size_t j = 0; j < elements_per_thread; j++) bag.insert((i * elements_per_thread) + j);
         }
 }
 
 void lock_based_for_all(auto& vec)
 {
-        auto foo = [](int& value)
+        auto foo = [](std::size_t& value)
         {
                 value = value * -1;
         };
 
 	#pragma omp parallel for
-        for(int& e: vec) foo(e);
+        for(std::size_t& e: vec) foo(e);
 }
 
 void wait_free_for_all(auto& bag)
 {
-        auto foo = [](int& value)
+        auto foo = [](std::size_t& value)
         {
                 value = value * -1;
         };
@@ -54,7 +52,7 @@ void wait_free_for_all(auto& bag)
 void lock_based_extract(auto& vec)
 {
 	#pragma omp parallel for
-        for(int i = 0; static_cast<decltype(vec.size())>(i) < vec.size(); i++)
+        for(std::size_t i = 0; static_cast<decltype(vec.size())>(i) < vec.size(); i++)
         {
                 while(vec.size() > 0)
                 {
@@ -64,24 +62,40 @@ void lock_based_extract(auto& vec)
         }
 }
 
-void wait_free_extract(auto& bag)
+void wait_free_extract(auto& bag, const std::size_t num_threads)
 {
 	#pragma omp parallel for
-        for(int i = 0; i < THREADS; i++)
+        for(std::size_t i = 0; i < num_threads; i++)
         {
                 while(bag.size() > 0) bag.extract();
         }
 }
 
-int main()
+int main(int argc, char** argv)
 {
-        wait_free_bag::WaitFreeBag<int, THREADS> bag;
-        std::vector<int>                         vec;
+        if(argc <= 1)
+        {
+                std::println("[ERROR] Incorrect usage...");
+                std::println("Usage: evaluate <number of elements>");
+                std::exit(-1);
+        }
+
+        std::size_t num_threads = 0;
+	#pragma omp parallel master
+        num_threads = omp_get_num_threads();
+
+        std::stringstream ss(argv[1]);
+        std::size_t       num_elements = 0;
+        ss >> num_elements;
+        const std::size_t elements_per_thread = num_elements / num_threads;
+
+        wait_free_bag::WaitFreeBag<std::size_t, 16> bag;
+        std::vector<std::size_t>                    vec;
 
         const auto tp0 = std::chrono::high_resolution_clock::now();
-        lock_based_insert(vec);
+        lock_based_insert(vec, num_threads, elements_per_thread);
         const auto tp1 = std::chrono::high_resolution_clock::now();
-        wait_free_insert(bag);
+        wait_free_insert(bag, num_threads, elements_per_thread);
         const auto tp2 = std::chrono::high_resolution_clock::now();
         lock_based_for_all(vec);
         const auto tp3 = std::chrono::high_resolution_clock::now();
@@ -89,7 +103,7 @@ int main()
         const auto tp4 = std::chrono::high_resolution_clock::now();
         lock_based_extract(vec);
         const auto tp5 = std::chrono::high_resolution_clock::now();
-        wait_free_extract(bag);
+        wait_free_extract(bag, num_threads);
         const auto tp6 = std::chrono::high_resolution_clock::now();
 
         const std::chrono::duration<double> lock_based_insert_time  = tp1 - tp0;
@@ -99,21 +113,16 @@ int main()
         const std::chrono::duration<double> lock_based_extract_time = tp5 - tp4;
         const std::chrono::duration<double> wait_free_extract_time  = tp6 - tp5;
 
-        int num_threads = -1;
-	#pragma omp parallel
-        {
-	        #pragma omp master
-                num_threads = omp_get_num_threads();
-        }
-
-        std::println("{},{},{},{},{},{},{},{},{},{}",
+        std::println("{},{},{},{},{},{},{},{},{},{},{},{}",
                      num_threads,
-                     lock_based_insert_time,
-                     wait_free_insert_time,
-                     lock_based_for_all_time,
-                     wait_free_for_all_time,
-                     lock_based_extract_time,
-                     wait_free_extract_time,
+                     num_elements,
+                     elements_per_thread,
+                     lock_based_insert_time.count(),
+                     wait_free_insert_time.count(),
+                     lock_based_for_all_time.count(),
+                     wait_free_for_all_time.count(),
+                     lock_based_extract_time.count(),
+                     wait_free_extract_time.count(),
                      lock_based_insert_time / wait_free_insert_time,
                      lock_based_for_all_time / wait_free_for_all_time,
                      lock_based_extract_time / wait_free_extract_time);
