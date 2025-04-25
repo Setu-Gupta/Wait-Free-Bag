f657ff945adbf4662fc22bb001349f3c5a9014ec
Setu Gupta <setu.gupta.2020@gmail.com>
2025-04-25 11:04:35 -0500

    Fixed formatting
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   test/profile.cpp

--- test/profile.cpp
+++ test/profile.cpp
@@ -6,7 +6,7 @@ const std::size_t ELEMENTS_PER_THREAD = (1UZ << 60);
 
 void wait_free_insert(auto& bag)
 {
-#pragma omp parallel for
+        #pragma omp parallel for
         for(std::size_t i = 0; i < THREADS; i++)
         {
                 for(std::size_t j = 0; j < ELEMENTS_PER_THREAD; j++) bag.insert(static_cast<int>((i * ELEMENTS_PER_THREAD) + j));
@@ -20,13 +20,13 @@ void wait_free_for_all(auto& bag)
                 value = value * -1;
         };
 
-#pragma omp parallel
+        #pragma omp parallel
         bag.for_all(foo);
 }
 
 void wait_free_extract(auto& bag)
 {
-#pragma omp parallel for
+        #pragma omp parallel for
         for(std::size_t i = 0; i < THREADS; i++)
         {
                 while(bag.size() > 0) bag.extract();
